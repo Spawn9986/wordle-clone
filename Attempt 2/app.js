180b3d041d161b63d8025ed87ced16c370ecaa89
Spawn9986 <scouch9986@gmail.com>
2023-07-05 14:34:29 -0500

    created two variables to aid the addLetter function

--- Attempt 2/app.js
+++ Attempt 2/app.js
@@ -2,28 +2,38 @@
 //select the input boxes from the DOM and create a nodeList
 const letters = document.querySelectorAll(".scoreboard-letter");
 const loadingDiv = document.querySelector(".info-bar");
+const ANSWER_LENGTH = 5;
 
 //=============== LOGIC =======================
 
 //create a wrapping function that will allow us to do await wherever we want to later on
 async function init() {
+  const currentGuess = "";
+  function addLetter(letter) {
+    if (currentGuess.length < ANSWER_LENGTH) {
+    }
+  }
   document.addEventListener("keydown", function handleKeyPress(event) {
     //event.key to capture the users key entered
     const action = event.key;
-    console.log(action);
-  });
 
-  //if users key press is Enter -->
-  if (action === "Enter") {
-    //function for when a user is trying to commit a guess
-    commit();
-  } else if (action === "Backspace") {
-    backspace();
-  } else if (isLetter(action)) {
-    addLetter(action.toUppercase());
-  } else {
-    // do nothing
-  }
+    //if users key press is Enter -->
+    if (action === "Enter") {
+      //function for when a user is trying to commit a guess
+      commit();
+    } else if (action === "Backspace") {
+      backspace();
+    } else if (isLetter(action)) {
+      addLetter(action.toUpperCase());
+    } else {
+      // do nothing
+    }
+  });
+}
+
+//add a function to check whether or not the users key was a letter (a valid keystroke) using a regular expression
+function isLetter(letter) {
+  return /^[a-zA-Z]$/.test(letter);
 }
 
 init();
@@ -39,5 +49,7 @@ init();
 6 - Create functions for when the user keys "Enter" (is trying to commit a guess) 
 7 - Create functions for when the user keys "Backspace"
 8 - Create functions for when the user keys a letter (will also need a function to evaluate if it's a letter then another for what to do after if so).
+9 - Add function to letter conditional to evaluate whether user keyed a letter (i.e., a valid keystroke) 
+10 - Work on the addLetter function by creating/defining it as it was evoked in the handleKeyPress function previously. We will need a variable to store the length of the current guess for our conditional statement and another variable for what the max length of guess will be (constant; will not change) to compare it to.
 
 */
