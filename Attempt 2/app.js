5fde6782e736bcf30eedc79857dfbae6a6670121
Spawn9986 <scouch9986@gmail.com>
2023-07-05 13:56:30 -0500

    adding initial skeleton

--- Attempt 2/app.js
+++ Attempt 2/app.js
@@ -12,6 +12,18 @@ async function init() {
     const action = event.key;
     console.log(action);
   });
+
+  //if users key press is Enter -->
+  if (action === "Enter") {
+    //function for when a user is trying to commit a guess
+    commit();
+  } else if (action === "Backspace") {
+    backspace();
+  } else if (isLetter(action)) {
+    addLetter(action.toUppercase());
+  } else {
+    // do nothing
+  }
 }
 
 init();
@@ -22,5 +34,10 @@ init();
 1 - Select DOM elements
 2 - Add event listener with keydown function
 3 - Setup the keydown function by capturing the users key press and storing it into a variable
+4 - Make initial function delegate to other functions depending on circumstance by creating additional inner functions
+5 - Create if conditionals for keypress (Enter, backspace, letter)
+6 - Create functions for when the user keys "Enter" (is trying to commit a guess) 
+7 - Create functions for when the user keys "Backspace"
+8 - Create functions for when the user keys a letter (will also need a function to evaluate if it's a letter then another for what to do after if so).
 
 */
