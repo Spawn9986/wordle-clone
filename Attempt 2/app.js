28f1407e3798209590734fdd83522a56a30775e3
Spawn9986 <scouch9986@gmail.com>
2023-07-05 13:20:15 -0500

    addiing DOM selectors

--- Attempt 2/app.js
+++ Attempt 2/app.js
@@ -1,9 +1,8 @@
 //============= GLOBAL VARIABLES ==============
-
+//select the input boxes from the DOM and create a nodeList
+const letters = document.querySelectorAll(".scoreboard-letter");
+const loadingDiv = document.querySelector(".info-bar");
 
 //============= OVERVIEW FLOW =========================
 
-
-
 //============== SUPPORTING ELEMENTS ===================
-
