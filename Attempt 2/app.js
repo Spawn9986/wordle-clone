bbf2cdf7f7e22b19dbf66c00a82b1614550cff78
Spawn9986 <scouch9986@gmail.com>
2023-07-05 15:09:43 -0500

    progress on the addLetter function

--- Attempt 2/app.js
+++ Attempt 2/app.js
@@ -8,10 +8,18 @@ const ANSWER_LENGTH = 5;
 
 //create a wrapping function that will allow us to do await wherever we want to later on
 async function init() {
-  const currentGuess = "";
+  let currentGuess = "";
+
   function addLetter(letter) {
     if (currentGuess.length < ANSWER_LENGTH) {
+      currentGuess += letter;
+    } else {
+      //if on the last letter (last index not to exceed max) create a string that takes off the last letter and adds the new letter keyed by user
+      currentGuess =
+        currentGuess.substring(0, currentGuess.length - 1) + letter;
     }
+    //render the users keyed letters to the DOM. Since each index is 1 number behind (starts at index 0) we take the currentGuess character length - 1 to get the last available box; we also want the inner text;
+    letters[currentGuess.length - 1].innerText = letter;
   }
   document.addEventListener("keydown", function handleKeyPress(event) {
     //event.key to capture the users key entered
@@ -50,6 +58,10 @@ init();
 7 - Create functions for when the user keys "Backspace"
 8 - Create functions for when the user keys a letter (will also need a function to evaluate if it's a letter then another for what to do after if so).
 9 - Add function to letter conditional to evaluate whether user keyed a letter (i.e., a valid keystroke) 
-10 - Work on the addLetter function by creating/defining it as it was evoked in the handleKeyPress function previously. We will need a variable to store the length of the current guess for our conditional statement and another variable for what the max length of guess will be (constant; will not change) to compare it to.
+10 - Work on the addLetter function by creating/defining it as it was evoked in the handleKeyPress function previously. 
+    * We will need a variable to store the length of the current guess for our conditional statement
+    * We will need a variable to store the max length of letters for the users guess. It will be constant and will not change. We will use it to compare the previous variable of the length of the current guess to.
+    * create conditionals for whether the letter keyed is the last letter or not and respond accordingly
+    * finally, add the users keyed letters to the DOM
 
 */
