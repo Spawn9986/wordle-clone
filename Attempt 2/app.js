8e9723447f871e30596560668c5630a918c434eb
Spawn9986 <scouch9986@gmail.com>
2023-07-05 16:40:35 -0500

    added ability for user to have multiple attempts

--- Attempt 2/app.js
+++ Attempt 2/app.js
@@ -9,28 +9,48 @@ const ANSWER_LENGTH = 5;
 //create a wrapping function that will allow us to do await wherever we want to later on
 async function init() {
   let currentGuess = "";
+  //the DOM equivalent of what attempt (1 - 6) in the gameplay the user is on
+  let currentRow = 0;
 
   function addLetter(letter) {
     if (currentGuess.length < ANSWER_LENGTH) {
+      //add letter to the end
       currentGuess += letter;
     } else {
-      //if on the last letter (last index not to exceed max) create a string that takes off the last letter and adds the new letter keyed by user
+      //if on the last letter --> replace last letter
       currentGuess =
         currentGuess.substring(0, currentGuess.length - 1) + letter;
     }
-    //render the users keyed letters to each DOM elements innerText (update the visible text of the selected DOM element). Since indexes starts at 0 we take the currentGuess.length - 1 to get the index of the DOM element that corresponds to the current position of the guess:
-    //letters (from the querySelectAll) === DOM-Element
-    //letter (from the addLetter parameter) === usersKeyedLetter
-    //Thus, (the inner text of the DOM elements current index) = (the users keyed letter changed to uppercase); (same as let name[0] = "Jim; name[1] = "Ted"; name[2] = "Larry" but instead as DOM[0].innerText = "C"; DOM[1].innerText = "L"; DOM[2].innerText = "E")
-    letters[currentGuess.length - 1].innerText = letter;
+    //render the users keyed letters on the correlating DOM elements innerText:
+    //currentGuess.length - 1; indexes start at 0 (not 1). Thus, we get the index of the DOM element that corresponds to the current position of the users guess
+    //letters (from the querySelectAll) acting as the DOM element
+    //letter (from the addLetter parameter) acting as the usersKeyedLetter
+    // becomes --> letters[currentGuess.length - 1].innerText = letter;
+    //Where (the inner text of the DOM elements current index) = (the users keyed letter changed to uppercase);
+    //Same concept as let name[0] = "Jim; name[1] = "Ted"; name[2] = "Larry" but instead --> DOM[0].innerText = "C"; DOM[1].innerText = "L"; DOM[2].innerText = "E")
+    //to ensure we render on the DOM element of the current row (attempt) we add ANSWER_LENGTH (always 5) * currentRow (starting at 0) in front to account for what has already taken place. Thus, row 1: [5 * 0 + currentGuess] --> current Guess starts at 0; row 2: [5 * 1 + currentGuess] --> currentGuess starts at 6 etc.
+    letters[ANSWER_LENGTH * currentRow + currentGuess.length - 1].innerText =
+      letter;
   }
+
+  async function commit() {
+    if (currentGuess.length !== ANSWER_LENGTH) {
+      // do nothing
+      return;
+    }
+
+    currentRow++;
+    //new row, so reassign their current guess to empty string
+    currentGuess = "";
+  }
+
   document.addEventListener("keydown", function handleKeyPress(event) {
     //event.key to capture the users key entered
     const action = event.key;
 
     //if users key press is Enter -->
     if (action === "Enter") {
-      //function for when a user is trying to commit a guess
+      //User tries to commit a guess
       commit();
     } else if (action === "Backspace") {
       backspace();
@@ -61,10 +81,13 @@ init();
 7 - Create functions for when the user keys "Backspace"
 8 - Create functions for when the user keys a letter (will also need a function to evaluate if it's a letter then another for what to do after if so).
 9 - Add function to letter conditional to evaluate whether user keyed a letter (i.e., a valid keystroke) 
-10 - Work on the addLetter function by creating/defining it as it was evoked in the handleKeyPress function previously. 
+10 - define/ fill in the addLetter function as it was evoked in the handleKeyPress function previously. 
     * We will need a variable to store the length of the current guess for our conditional statement
     * We will need a variable to store the max length of letters for the users guess. It will be constant and will not change. We will use it to compare the previous variable of the length of the current guess to.
     * create conditionals for whether the letter keyed is the last letter or not and respond accordingly
     * finally, add the users keyed letters to the DOM
+11 - define/ fill in the commit function
+    * if users guess !== expected character length
+    * need a variable to track what row (likened to what attempt 1- 6) they are on in the DOM
 
 */
