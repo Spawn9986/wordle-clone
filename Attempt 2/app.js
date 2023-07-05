2181e45b96a5f5285f76e59d410eaa7631ddfd81
Spawn9986 <scouch9986@gmail.com>
2023-07-05 13:32:12 -0500

    setup a keydown function that captures the users key press

--- Attempt 2/app.js
+++ Attempt 2/app.js
@@ -3,6 +3,24 @@
 const letters = document.querySelectorAll(".scoreboard-letter");
 const loadingDiv = document.querySelector(".info-bar");
 
-//============= OVERVIEW FLOW =========================
+//=============== LOGIC =======================
 
-//============== SUPPORTING ELEMENTS ===================
+//create a wrapping function that will allow us to do await wherever we want to later on
+async function init() {
+  document.addEventListener("keydown", function handleKeyPress(event) {
+    //event.key to capture the users key entered
+    const action = event.key;
+    console.log(action);
+  });
+}
+
+init();
+
+//=================== FLOW ===================
+/* 
+
+1 - Select DOM elements
+2 - Add event listener with keydown function
+3 - Setup the keydown function by capturing the users key press and storing it into a variable
+
+*/
